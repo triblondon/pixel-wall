507d28b721e524be4e01176b2e60f22259d0e9eb
Andrew Betts <andrew@trib.tv>
2019-08-11 21:42:47 +0100

    WIP

---
+++ src/layers/text-image.ts
@@ -0,0 +1,47 @@
+import Layer, { LayerOptionsType } from './layer.js';
+import { PixelDataType, FrameDataType } from '../utils/led-matrix.js';
+import * as gm from 'gm';
+
+type OptionsType = LayerOptionsType & {
+	text: string,
+	color?: PixelDataType,
+	speed: number,
+	loop?: boolean
+}
+
+const TEXT_CANVAS_WIDTH = 300;
+
+class TextImage extends Layer {
+
+	private text: string;
+	private imageData: FrameDataType;
+	private dirty: boolean;
+
+	constructor(options: OptionsType) {
+		super(options.position.x, options.position.y, options.size.w, options.size.h);
+		this.text = '';
+		this.imageData = null;
+		this.dirty = false;
+	}
+	async setText(str: string) {
+		this.text = str;
+		await new Promise(resolve => {
+			gm(TEXT_CANVAS_WIDTH, this.size.h, "#000000ff").fill("#ffffff").font('Arial.ttf', 9).drawText(0, 0, str, "NorthWest").toBuffer('RGB', (err, buffer) => {
+				this.imageData = [...buffer].reduce((acc, colorVal, idx) => {
+					if (idx % 3 === 0) acc.push([]);
+					acc[acc.length - 1][idx % 3] = colorVal;
+					return acc;
+				}, []);
+				this.dirty = true;
+				resolve();
+			})
+		});
+	}
+	frame() {
+		if (this.dirty) {
+			return this.imageData;
+		}
+	}
+}
+
+module.exports = TextImage;
