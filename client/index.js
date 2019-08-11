b776c0ceaf01837f6a3e1baced6c80c67350524f
Andrew Betts <andrew@trib.tv>
2019-08-11 21:54:25 +0100

    WIP

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import fire from '../lib/models/fire.ts';
+
+var canvas = document.getElementById('tutorial');
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'rgb(200, 0, 0)';
+ctx.fillRect(10, 10, 50, 50);
+
+ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+ctx.fillRect(30, 30, 50, 50);
