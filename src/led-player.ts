1c2db98e902ee4350edcd31eff9460efe24c39ae
Andrew Betts <andrew@trib.tv>
2019-08-18 13:50:18 +0100

    CLI renaming

--- src/led-player.ts
+++ src/led-player.ts
@@ -1,9 +1,9 @@
-import fireScene from './scenes/fire';
+import scene from './scenes/fire';
 import { init, render } from './utils/ws281x-renderer';
 import signalHandler from './utils/signal-handler';
 
-signalHandler.on(['int', 'term'], () => fireScene.setAll(0,0,0).render());
+signalHandler.on(['int', 'term'], () => scene.setAll(0,0,0).render());
 
-init(fireScene.rows, fireScene.cols, 0.5);
+init(scene.rows, scene.cols, 0.5);
 
-fireScene.useRenderer(render)
+scene.useRenderer(render)
