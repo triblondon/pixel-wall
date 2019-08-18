3857e26400bcf9273e1d2e763764a88acc2ef626
Andrew Betts <andrew@trib.tv>
2019-08-18 13:58:26 +0100

    fix dir ref

--- src/scenes/fire.ts
+++ src/scenes/fire.ts
@@ -7,7 +7,7 @@ import * as path from 'path';
 const fire = new Slideshow({ size: { w: 12, h: 12 }, position: { x: 0, y: 0} });
 const matrix = new Matrix({ rows: 12, cols: 12, frameRate: 20 });
 
-const dirName = path.join(__dirname, '../images/fire');
+const dirName = path.join(__dirname, '../../images/fire');
 fs.
 	readdirSync(dirName).
 	filter(name => name.endsWith('.rgb'))
